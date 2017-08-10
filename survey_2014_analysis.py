abab6dae7edfd8dde39821e37e6e6654480501a4
SimonHettrick <s.hettrick@software.ac.uk>
2017-08-10 23:46:50 +0100

    Playing with plots

--- survey_2014_analysis.py
+++ survey_2014_analysis.py
@@ -94,61 +94,60 @@ def get_counts(df,grouped_cols):
 
     return univariate_summary_dfs
 
-def plot_basic_charts(dict_of_dfs):
+
+def plot_basic_seaborn(dict_of_dfs):
     """
     Create a basic plot for each question. Plots of more specific interest will
     be created in a separate function, because it's impossible to automate it.
+    Uses Seaborn to try and make things prettier
     :params: a dict of dataframe, the imported plot details
     :return: A list of saved charts
     """
-
-    for key in dict_of_dfs:
-        df_temp = dict_of_dfs[key]
-        count_colname = df_temp.columns[0]
-        percent_colname = df_temp.columns[1]
-
-        title = plot_details[key][0]
-        labels = df_temp.index
-        labels = [ '\n'.join(wrap(l, 12)) for l in labels ]
-        plt.subplots_adjust(bottom=.3)
-
-        # Create plot for counts
-        fig = df_temp[count_colname].plot(kind='bar',legend=None,title=title)
-        fig.set_xticks(df_temp.index)
-        fig.set_xticklabels(labels)
-        
-
-        plt.ylabel('Count')
-        plt.savefig(STOREFILENAME + 'basic_counts/' + key + '.png', format = 'png', dpi = 150)
-        # Need to clear the frame otherwise I just get the same
-        # plot stored twice!
-        plt.clf()
-        # Create plots for percentages
-        df_temp[percent_colname].plot(kind='bar',legend=None,title=title)
-        plt.ylabel('Percentage')
-        plt.savefig(STOREFILENAME + 'basic_percentage/' + key + '.png', format = 'png', dpi = 150)
-    return
-
-
-def plot_basic_seaborn(dict_of_dfs):
 
     for key in dict_of_dfs:
         # Read the dfs one at the time
         df_temp = dict_of_dfs[key]
         # Title's from the lookup table
         title = plot_details[key][0]
+        # Set columns for the first and second plot
         count_colname = df_temp.columns[0]
-        # Some of the labels are really long
-        # we need to cut them up
-        labels = df_temp.index
-        labels = [ '\n'.join(wrap(l, 12)) for l in labels ]
-        # Now plot
+        percent_colname = df_temp.columns[1]
+
+        # Some plots shouldn't be ordered by size. These are identified
+        # in the plot_details lookup table and the new order (for both the
+        # plot labels and - of course - the order of the dataframe) is
+        # described in the reordered_axes lookup table
+        if plot_details[key][1] == True:
+            labels = reordered_axes[key]
+            df_temp = df_temp.reindex(reordered_axes[key])
+        else:
+            labels = df_temp.index
+            
+        # Some of the labels are really long so I cut them up
+        labels = [ '\n'.join(wrap(l, 15)) for l in labels ]
+
+        # Now plot first plot
         sns.barplot(x = labels, y = df_temp[count_colname], data = df_temp).set_title(title)
-        plt.subplots_adjust(bottom=.3)
-#        plt.tight_layout()
+        # Make gap at bottom bigger for labels (it's a fraction, not a measurement)
+        plt.subplots_adjust(bottom=0.3)
         plt.ylabel('Count')
         plt.xticks(rotation=90)
         plt.savefig(STOREFILENAME + 'basic_counts/' + key + '.png', format = 'png', dpi = 150)
+#        plt.show()
+        # Funnliy enough, Seaborn seems a bit sticky. There's a weird kind of
+        # colour bleed from one plot to the next. However, by explicitly clearing the 
+        # frame in the following step, it's all sorted
+        plt.clf()
+
+        # Now plot second plot
+        sns.barplot(x = labels, y = df_temp[percent_colname], data = df_temp).set_title(title)
+        plt.subplots_adjust(bottom=.3)
+        plt.ylabel('Percentage')
+        plt.xticks(rotation=90)
+        plt.savefig(STOREFILENAME + 'basic_percentage/' + key + '.png', format = 'png', dpi = 150)
+#        plt.show()
+        # Clearing the frame as described above
+        plt.clf()
 
     return
 
@@ -170,10 +169,10 @@ def main():
     # Store results in dict of dfs
     univariate_summary_dfs = get_counts(df,grouped_cols)
 
-    plot_basic_charts(univariate_summary_dfs)
+    # Plot all univariate stuff with no frills
+    plot_basic_seaborn(univariate_summary_dfs)
 
 
-#    plot_basic_seaborn(univariate_summary_dfs)
 
 
 if __name__ == '__main__':
